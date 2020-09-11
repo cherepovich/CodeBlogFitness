8133fba9b9ab2d4ac6c1ff673df25fcda5c8aff0
Vladimir Cherepovich <v.cherepovich@gmail.com>
2020-09-11 23:24:39 +0500

    Создана модель пользователя

---
+++ CodeBlogFithess.BL/Model/User.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace CodeBlogFithess.BL.Model
+{
+    /// <summary>
+    /// Пользователь
+    /// </summary>
+    class User
+    {
+        #region Свойства класса
+        public string Name { get; } // Публичное свойство - "правильно обернутые глобальные переменные"
+        public Gender Gender { get; } // Пол человека. Не даем возможности изменять
+        public DateTime BirthDate { get; }
+        public Double Weight { get; set; }
+        public Double Height { get; set; }
+        #endregion
+
+        /// <summary>
+        /// Создание пользователя
+        /// </summary>
+        /// <param name="name">параметр - имя пользователя</param>
+        /// <param name="gender"></param>
+        /// <param name="birthDate"></param>
+        /// <param name="weight"></param>
+        /// <param name="height"></param>
+        public User(string name, Gender gender, DateTime birthDate, Double weight, Double height)
+        {
+            #region Проверка условий
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentNullException("Имя пользователя не может быть пустым", nameof(name));
+            }
+
+            if (gender == null)
+            {
+                throw new ArgumentNullException("Пол пользователя не может быть null", nameof(gender));
+            }
+
+            if (birthDate < DateTime.Parse("01.01.1900") || birthDate >= DateTime.Now)
+            {
+                throw new ArgumentException("Невозможная дата рождения", nameof(birthDate));
+            }
+
+            if (weight <= 0)
+            {
+                throw new ArgumentException("Неверный вес", nameof(weight));
+            }
+
+            if (height <= 0)
+            {
+                throw new ArgumentException("Неверный рост", nameof(height));
+            }
+            #endregion
+
+            Name = name;
+            Gender = gender;
+            BirthDate = birthDate;
+            Weight = weight;
+            Height = height;
+        }
+        public override string ToString()
+        {
+            return Name;
+        }
+
+
+    }
+}
